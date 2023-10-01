ec10bd0880bb8548075b87b418ccb48e52762cf8
click-SK <sashakushnir4991@gmail.com>
2023-10-01 19:20:38 +0300

    add fix style

--- compomemts/CallToAction.jsx
+++ compomemts/CallToAction.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../style/callToAction.module.scss";
 import Image from 'next/image'
+import ImgFonFirstSection from '../public/img/fon-img-firsht-second2.png'
 const CallToAction = () => {
   return (
     <div className={styles.call_to_action_wrap}>
@@ -29,11 +30,11 @@ const CallToAction = () => {
         </div>
       </div>
       <div className={styles.call_to_action_img_wrap}>
-        <Image className={styles.call_to_action_img} 
-        src="./img/call-to-action.svg" 
+        {/* <Image className={styles.call_to_action_img} 
+        src={ImgFonFirstSection} 
         alt="call-to-action"
         width={1000}
-        height={1000}/>
+        height={1000}/> */}
       </div>
     </div>
   );
--- compomemts/Header.jsx
+++ compomemts/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from '../style/header.module.scss';
 import Link from "next/link";
+import ChangeColor from "./ChangeColor/ChangeColor";
+import Image from 'next/image'
+import Logo from '../public/logo-header.svg'
 const Header = () => {
   return (
     <header className={styles.header_wrap}>
       <div className={styles.logo}>
-        <Link href='/'>Logo</Link>
+        <Link href='/'>
+            <Image  
+            className={styles.header_logo}
+            src={Logo} 
+            alt="Logo"
+            width={1000}
+            height={1000}/>
+        </Link>
       </div>
       <nav className={styles.nav_block}>
         <ul className={styles.nav_wrap}>
@@ -16,6 +26,7 @@ const Header = () => {
           <Link href='/contsct'>Контакти</Link>
         </ul>
       </nav>
+      <ChangeColor/>
     </header>
   );
 };
