ac1668bc9540b7eb69a06474353e058b7e3d1107
ros455 <ros.kichuk@gmail.com>
2023-10-04 00:22:29 +0300

    fix theme bug 04.10 -ros

--- hooks/use-thems.jsx
+++ hooks/use-thems.jsx
@@ -1,16 +1,45 @@
-'use client'
+// import { useLayoutEffect, useState } from "react"
+
+// // const isDarkTheme = window?.matchMedia('(prefers-color-scheme:dark)').matches
+// // const defaultTheme = isDarkTheme ? 'dark_grey_pink' : 'yelow-white'
+
+
+// export const useTheme = () =>{
+//     // const [theme, setTheme] = useState(localStorage.getItem('app-them') || defaultTheme)
+//     const [theme, setTheme] = useState(localStorage.getItem('app-them'))
+
+//     console.log('theme',localStorage.getItem('app-them'));
+
+//     useLayoutEffect(() =>{
+//         document.documentElement.setAttribute('data-theme', theme)
+//         localStorage.setItem('app-them', theme)
+//     }, [theme])
+//     return {theme, setTheme}
+// }
+
 import { useLayoutEffect, useState } from "react"
 
-const isDarkTheme = window?.matchMedia('(prefers-color-scheme:dark)').matches
-const defaultTheme = isDarkTheme ? 'dark_grey_pink' : 'yelow-white'
+export const useTheme = () => {
+    const [theme, setTheme] = useState(null); 
 
+    useLayoutEffect(() => {
+        // Зчитування теми з localStorage під час монтування компоненту
+        const storedTheme = localStorage.getItem('app-them');
+        if (storedTheme) {
+            setTheme(storedTheme);
+        } else {
+            setTheme(defaultTheme);
+            localStorage.setItem('app-them', defaultTheme);
+        }
 
-export const useTheme = () =>{
-    const [theme, setTheme] = useState(localStorage.getItem('app-them') || defaultTheme)
+    }, []);
 
-    useLayoutEffect(() =>{
-        document.documentElement.setAttribute('data-theme', theme)
-        localStorage.setItem('app-them', theme)
-    }, [theme])
-    return {theme, setTheme}
+    useLayoutEffect(() => {
+        if (theme !== null) {
+            document.documentElement.setAttribute('data-theme', theme);
+            localStorage.setItem('app-them', theme);
+        }
+    }, [theme]);
+
+    return { theme, setTheme };
 }
